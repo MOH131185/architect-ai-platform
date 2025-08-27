f4662d2136d705f99faf927bb31ff9f908694e1f
MOH131185 <reggabmohammed@gmail.com>
2025-08-27 10:57:09 +0100

    Revert "Fix fetching and display of UK zoning data"

--- api/proxy-planning.js
+++
@@ -1,9 +0,0 @@
-// api/proxy-planning.js (Vercel or Netlify function)
-export default async function handler(req, res) {
-  const { lat, lng } = req.query;
-  const url = `https://planning.data.gov.uk/api/v1/constraints?lat=${lat}&lon=${lng}`;
-  const response = await fetch(url);
-  const text = await response.text();
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.status(200).send(text);
-}
